506c01508a903d6e9afd64f3391f8dd13e5d9f6d
dalailai <1124598739@qq.com>
2019-11-21 20:58:42 +0800

    --

--- app/src/main/java/com/jy/day_05/adapter/RecAdapter.java
+++ app/src/main/java/com/jy/day_05/adapter/RecAdapter.java
@@ -7,7 +7,6 @@ import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
